d17c041bc07c1770ee3fa4d8926c7fb93170425b
Brian Kim <brianjunewookim@gmail.com>
2019-11-20 18:53:55 -0800

    Move board generation to board initializer

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,18 +2,10 @@ import React from 'react';
 
 import Rows from './Rows';
 
-const Board = ({ selector }) => {
-  const squares = [];
-  for (let i = 0; i < 8; i++) {
-    squares.push([]);
-    for (let j = 0; j < 8; j++) {
-      squares[i].push([i, j]);
-    }
-  }
-
+const Board = ({ board, selector }) => {
   return(
     <div style={boardStyle}>{
-      squares.map(row => {
+      board.map(row => {
         return <Rows row={row} selector={selector} />
       })
     }</div>
