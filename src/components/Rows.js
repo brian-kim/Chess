1c29526e766eb99127a3c7f6bf1a962543e3a69f
Brian Kim <brianjunewookim@gmail.com>
2019-11-20 17:19:49 -0800

    Create rows component for board

---
+++ src/components/Rows.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Squares from './Squares';
+
+const Rows = ({ row, selector }) => {
+  return(
+    <div style={rowStyle}>{row.map(square => {
+      return <Squares square={square} selector={selector} />
+    })}
+  </div>
+  )
+}
+
+const rowStyle = {
+  display: 'flex',
+  flexDirection: 'row'
+}
+
+export default Rows;
