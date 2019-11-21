a947a7540ff2d405ca974de4f1c072e356ad75db
Brian Kim <brianjunewookim@gmail.com>
2019-11-20 18:55:42 -0800

    Add pawn to display on square component

--- src/components/Squares.js
+++ src/components/Squares.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 const Squares = ({ square, selector }) => {
   let backgroundColor;
-  const reduced = square.reduce((acc, curr) => acc + curr);
+  const reduced = square[0] + square[1]
   reduced % 2 === 0 ? backgroundColor = 'lightGrey' : backgroundColor = 'grey';
   
   const squareStyle = {
     width: '50px',
     height: '50px',
+    fontSize: '40px',
     backgroundColor: backgroundColor,
     border: '1px solid black',
     textAlign: 'center'
   }
 
   return(
-    <div style={squareStyle} onClick={() => selector(square)}></div>
+    <div style={squareStyle} onClick={() => selector(square)}>{square[2]}</div>
   )
 }
 
