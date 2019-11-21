09528fbaa56c99b2231717d2a9a9431097a35ff7
Brian Kim <brianjunewookim@gmail.com>
2019-11-20 18:54:40 -0800

    Rename to pieceMover.js

--- src/utils/boardUpdater.js
+++
@@ -1,9 +0,0 @@
-const boardUpdater = (gameBoard, squareCoordinate) => {
-  const xCoord = squareCoordinate[1];
-  const yCoord = squareCoordinate[0];
-  const updatedBoard = gameBoard;
-  updatedBoard[yCoord][xCoord]++;
-  return updatedBoard;
-}
-
-export default boardUpdater;
