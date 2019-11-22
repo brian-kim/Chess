4744185ac80d91a79bcb51b2aa949de05bf621df
Brian Kim <brianjunewookim@gmail.com>
2019-11-21 17:39:15 -0800

    Add rest of chess pieces to board initialization

--- src/utils/boardInitializer.js
+++ src/utils/boardInitializer.js
@@ -2,18 +2,61 @@ const boardInitializer = () => {
   const squares = [];
   for (let i = 0; i < 8; i++) {
     squares.push([]);
-    if (i === 1) {
+    // add rest of black pieces
+    if (i < 1) {
+      for (let j = 0; j < 8; j++) {
+        // add black rooks
+        if (j === 0 || j === 7) {
+          squares[i].push([i, j, 'bR']);
+        // add black bishops
+        } else if (j === 1 || j === 6) {
+          squares[i].push([i, j, 'bB']);
+        // add black knights
+        } else if (j === 2 || j === 5) {
+          squares[i].push([i, j, 'bN']);
+        // add black queen
+        } else if (j === 3) {
+          squares[i].push([i, j, 'bQ']);
+        // add black king
+        } else if (j === 4) {
+          squares[i].push([i, j, 'bK']);
+        }
+      }
+    // add black pawns
+    } else if (i < 2) {
       for (let j = 0; j < 8; j++) {
         squares[i].push([i, j, 'bP']);
       }
+    // add rest of the empty spaces
+    } else if (i < 6) {
+        for (let j = 0; j < 8; j++) {
+          squares[i].push([i, j, null]);
+        }
     }
-    else if (i === 6) {
+    //add white pawns
+    else if (i < 7) {
       for (let j = 0; j < 8; j++) {
         squares[i].push([i, j, 'wP']);
-      }
-    } else {
+      } 
+    // add rest of white pieces
+    } else if (i < 8) {
       for (let j = 0; j < 8; j++) {
-        squares[i].push([i, j, null]);
+        // add white rooks
+        if (j === 0 || j === 7) {
+          squares[i].push([i, j, 'wR']);
+        // add white bishops
+        } else if (j === 1 || j === 6) {
+          squares[i].push([i, j, 'wB']);
+        // add white knights
+        } else if (j === 2 || j === 5) {
+          squares[i].push([i, j, 'wN']);
+        // add white queen
+        } else if (j === 3) {
+          squares[i].push([i, j, 'wQ']);
+        // add white king
+        } else if (j === 4) {
+          squares[i].push([i, j, 'wK']);
+        }
       }
     }
   }
