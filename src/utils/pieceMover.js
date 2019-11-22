660a7021bee1c3bc5846e5508d7cb589d5ddb2e8
Brian Kim <brianjunewookim@gmail.com>
2019-11-21 17:13:27 -0800

    Add check for valid movement for pieces

--- src/utils/pieceMover.js
+++ src/utils/pieceMover.js
@@ -1,17 +1,45 @@
+import Pawn from '../Pieces/pawn';
+
 const pieceMover = (gameBoard, curSquare, newSquare) => {
   const curX = curSquare[1];
   const curY = curSquare[0];
-  const curPiece = curSquare[2];
+  const curSquarePiece = curSquare[2];
   const newX = newSquare[1];
   const newY = newSquare[0];
-  if (curPiece === 'P') {
-    if (newX - curX === 0 && newY - curY === -1) {
-      gameBoard[newY][newX][2] = curPiece;
-      gameBoard[curY][curX][2] = null;
-    } else {
-      alert('Not a valid move');
-    }
+
+  const invalidMoveMessage = () => alert('Not a valid move');
+
+  switch(curSquarePiece) {
+    case 'wP':
+      if (Pawn.checkMove(gameBoard, curX, curY, newX, newY)) {
+        gameBoard[newY][newX][2] = curSquarePiece;
+        gameBoard[curY][curX][2] = null;
+      } else {
+        invalidMoveMessage();
+      }
+      break;
+    case 'bP':
+      if (Pawn.checkMove(gameBoard, curX, curY, newX, newY)) {
+        gameBoard[newY][newX][2] = curSquarePiece;
+        gameBoard[curY][curX][2] = null;
+      } else {
+        invalidMoveMessage();
+      }
+      break;
+    // check movement rooks
+    
+    // check movement bishops
+  
+    // check movement knight
+  
+    // check movement kings
+  
+    // check movement queens
+
+    default:
+      console.log('pieceMover defaulted');
   }
+
   return gameBoard
 }
 
