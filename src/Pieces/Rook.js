950e9bb2950fd0ae8421f279e2b48aafb1119f6d
Brian Kim <brianjunewookim@gmail.com>
2019-11-22 15:50:21 -0800

    Correct bug in rook down movement checking

--- src/Pieces/Rook.js
+++ src/Pieces/Rook.js
@@ -1,51 +1,50 @@
 export default class Rook {
   static checkMove(gameBoard, curX, curY, newX, newY) {
-    const curSquarePiece = gameBoard[curY][curX][2];
     const xDif = newX - curX;
     const yDif = newY - curY;
 
-    if (curSquarePiece === 'wR' || curSquarePiece === 'bR') {
-      // piece moves vertically
-      if (xDif === 0) {
-        // piece moves up the board
-        if (yDif < 0) {
-          // checks all spaces up to new square to make sure it's not blocked
-          for (let i = -1; i > yDif; i--) {
-            if (gameBoard[curY + i][curX][2] !== null) {
-              return false;
-            }
+    // piece moves vertically
+    if (xDif === 0) {
+      // piece moves up the board
+      if (yDif < 0) {
+        // checks all spaces up to new square to make sure it's not blocked
+        for (let i = -1; i > yDif; i--) {
+          if (gameBoard[curY + i][curX][2] !== null) {
+            return false;
           }
-          return true;
-        // piece moves down the board
-        } else if (yDif > 0) {
-          for (let i = 1; i < yDif - 1; i++) {
-            if (gameBoard[curY + i][curX][2] !== null) {
-              return false;
-            }
-          }
-          return true;
         }
-      // piece moves horizontally
-      } else if (yDif === 0) {
-        if (xDif < 0) {
-          for (let i = -1; i > xDif; i--) {
-            if (gameBoard[curY][curX + i][2] !== null) {
-              return false;
-            }
+        return true;
+      // piece moves down the board
+      } else if (yDif > 0) {
+        for (let i = 1; i < yDif; i++) {
+          if (gameBoard[curY + i][curX][2] !== null) {
+            return false;
           }
-          return true;
-        } else if (xDif > 0) {
-          for (let i = 1; i < xDif; i++) {
-            if (gameBoard[curY][curX + i][2] !== null) {
-              return false;
-            }
-          }
-          return true;
         }
-      // piece didn't move
-      } else {
-        return false;
+        return true;
       }
+    // piece moves horizontally
+    } else if (yDif === 0) {
+      // piece is moving left
+      if (xDif < 0) {
+        for (let i = -1; i > xDif; i--) {
+          if (gameBoard[curY][curX + i][2] !== null) {
+            return false;
+          }
+        }
+        return true;
+      // piece is moving right
+      } else if (xDif > 0) {
+        for (let i = 1; i < xDif; i++) {
+          if (gameBoard[curY][curX + i][2] !== null) {
+            return false;
+          }
+        }
+        return true;
+      }
+    // piece didn't move or made invalid move
+    } else {
+      return false;
     }
   }
 }
