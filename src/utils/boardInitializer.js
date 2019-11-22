b91f050965a5f5ad8656428ce5d49ee1a8aec20d
Brian Kim <brianjunewookim@gmail.com>
2019-11-21 17:12:52 -0800

    Add logic for black/white pawns when initializing board

--- src/utils/boardInitializer.js
+++ src/utils/boardInitializer.js
@@ -2,9 +2,14 @@ const boardInitializer = () => {
   const squares = [];
   for (let i = 0; i < 8; i++) {
     squares.push([]);
-    if (i === 6) {
+    if (i === 1) {
       for (let j = 0; j < 8; j++) {
-        squares[i].push([i, j, 'P']);
+        squares[i].push([i, j, 'bP']);
+      }
+    }
+    else if (i === 6) {
+      for (let j = 0; j < 8; j++) {
+        squares[i].push([i, j, 'wP']);
       }
     } else {
       for (let j = 0; j < 8; j++) {
