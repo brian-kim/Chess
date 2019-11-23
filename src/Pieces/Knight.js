aabcd72a41005b21e96adbbda07b2ab57fbcda40
Brian Kim <brianjunewookim@gmail.com>
2019-11-22 16:33:21 -0800

    Add clarification comments to knight logic

--- src/Pieces/Knight.js
+++ src/Pieces/Knight.js
@@ -3,10 +3,13 @@ export default class Knight {
     const xDif = Math.abs(newX - curX);
     const yDif = Math.abs(newY - curY);
 
+    // check if piece is moving 2 spaces left/right and 1 space up/down
     if (xDif === 2 && yDif === 1) {
       return true;
+    // check if piece is moving 2 spaces up/down and 1 space left/right
     } else if (yDif === 2 && xDif === 1) {
       return true;
+    // piece didn't move or made invalid move
     } else {
       return false;
     }
